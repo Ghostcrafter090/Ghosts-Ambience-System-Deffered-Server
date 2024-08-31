b624a11e29e527261ac14567ec3e1b8f0c687b30
Ghostcrafter090 <Joshpumpkinhead@icloud.com>
2024-08-31 14:23:51 -0300

    Removed junk.

--- disableanti.bat
+++
@@ -1,8 +0,0 @@
-:loop
-for /f "tokens=2 delims=:" %%a in ('powershell -command "Get-MpPreference" ^| bash -c "grep -e \"DisableRealtimeMonitoring\""') do (
-	if "$%%a"=="$ False" (
-		powershell -command "Set-MpPreference -DisableRealtimeMonitoring 0"
-	)
-)
-timeout /t 10
-goto loop
